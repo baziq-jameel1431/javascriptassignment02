89839dcbb76ece73679fd3365f9a342b2e6d3ad1
baziq <baziq.jameel1431@gmail.com>
2023-09-12 17:52:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,172 @@
+// Chapter No 06 
+// Q 01
+x++;
+// Q 02
+var x = 100;
+--x;
+
+// Q 03
+var x = 50;
+var y = x++;
+// value of y=50
+
+// Q 04
+ var y = 50;
+var z = --y;
+// value of z=49
+
+// Q 05
+var newNum = 100;
+--newNum;
+
+// Q 06
+var x = 50;
+x=x+1;
+
+// Q 07
+var x = 100;
+++x;
+alert(x);
+
+// Chapter N0 07
+// Q 01
+var calculatedNum = 2 + (2 * 6);
+// calculatedNum = 14
+
+// Q 02
+var calculatedNum = (2 + 2) * 6;
+// calculatedNum = 24
+
+// Q 03
+var calculatedNum = (2 + 2) * (4 + 2);
+// calculatedNum = 24
+
+// Q 04
+var calculatedNum = ((2 + 2) * 4) + 2;
+// calculatedNum = 18
+
+// Q 05
+var cost = (2+2)*(4+10);
+// producing the result 56
+
+// Q 06
+var units = 2+(2*4)+10;
+// Producing the result 20
+
+// Q 07
+var pressure = 4 / (2 * 4);
+// This arrangement of numbers cant produce result of 5
+
+// Chapter No 08
+
+// Q 01
+var num = "2"+"2";
+// value of num is 22
+
+// Q 02
+var message = ("Hello," + "Dolly");
+alert(message);
+
+// Q 03
+
+alert("33" + 3);
+// result is 333
+
+// Q 04
+alert("Pakistan" + "Zindabad");
+
+// Q 05
+var x = ("Hello" +2);
+alert(x);
+
+// Q 06
+var a = "Hello"
+var b = "World"
+var c =a+b;
+alert(c);
+
+// Chapter No 09
+// Q 01
+var firstName = prompt("Enter First Name");
+// Q 02
+var country = prompt("Country","China");
+// Q 03
+var yourName = prompt("Enter Your Name");
+// Q 04
+var message = prompt("hey!","Default");
+// Q 05
+var question = "What is your favorite color?";
+var defaultResponse = "Blue";
+var userResponse = prompt(question, defaultResponse);
+console.log("User's response: " + userResponse);
+// Q 06
+var question = "What is your favorite color?";
+var defaultResponse = "Blue";
+var userResponse = prompt(question, defaultResponse);
+alert("User's response: " + userResponse);
+
+// Chapter No 10
+// Q 01
+var city = "Karachi";
+if (city = "Karachi") {
+console.log("The City OF Lights");}
+// Q 02
+var x =prompt("Enter value of x");
+var y =prompt("Enter value of y");
+if (x === y){
+    var z = prompt("Enter value of z");
+    var t = z;
+    console.log(t);
+}
+// Q 03
+var zipCode = prompt("Enter zipCode");
+if(zipCode==="10010"){
+    alert("karachi");
+}
+else{
+    alert("please write correct city");
+}
+// Q 04
+var x = 1;
+if (x === 1) {
+     x = 2;
+}
+console.log("x is now: " + x);
+
+// Chapter No 11
+// Q 01
+if (variable1 !== variable2) {
+}
+// Q 02
+if (variable1 >= variable2) {
+
+}
+// Q 03
+var x = 5;
+if (x !== 10) {
+   
+    x = 10;
+}
+console.log("x is now: " + x);
+// Q 04
+var number1 = 5;
+var number2 = 10;
+if (number1 !== number2) {
+    alert("Congratulations!");
+}
+// Q 05
+var firstName = prompt("Please enter your first name:");
+var anotherName = "John"; 
+if (firstName !== anotherName) {
+  alert("No match");
+}
+
+
+
+
+
+
+
+
+
+
